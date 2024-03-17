1af66d800c226c1fb9e5464188d306b07d24217d
liangjk <junkaili@usc.edu>
2024-03-17 16:55:59 +0000

    Total Test Rerun at each time

--- src/test.sh
+++ src/test.sh
@@ -8,25 +8,25 @@ for i in {1..200}; do
 		echo -n "~"
 	done
 	echo -e "\nTest Raft"
-	time go test ./raft -v
+	time go test ./raft -v -count=1
 
 	for j in {1..80}; do
 		echo -n "~"
 	done
 	echo -e "\nTest KVRaft"
-	time go test ./kvraft -timeout 15m -v
+	time go test ./kvraft -timeout 15m -v -count=1
 
 	for j in {1..80}; do
 		echo -n "~"
 	done
 	echo -e "\nTest ShardCtrler"
-	time go test ./shardctrler -v
+	time go test ./shardctrler -v -count=1
 
 	for j in {1..80}; do
 		echo -n "~"
 	done
 	echo -e "\nTest ShardKV"
-	time go test ./shardkv -v
+	time go test ./shardkv -v -count=1
 
 	for j in {1..80}; do
 		echo -n "*"
@@ -37,23 +37,23 @@ for i in {1..200}; do
 		echo -n "~"
 	done
 	echo -e "\nTest Raft Race"
-	time go test ./raft -race -timeout 20m -v
+	time go test ./raft -race -timeout 20m -v -count=1
 
 	for j in {1..80}; do
 		echo -n "~"
 	done
 	echo -e "\nTest KVRaft Race"
-	time go test ./kvraft -race -timeout 20m -v
+	time go test ./kvraft -race -timeout 20m -v -count=1
 
 	for j in {1..80}; do
 		echo -n "~"
 	done
 	echo -e "\nTest ShardCtrler Race"
-	time go test ./shardctrler -race -v
+	time go test ./shardctrler -race -v -count=1
 
 	for j in {1..80}; do
 		echo -n "~"
 	done
 	echo -e "\nTest ShardKV Race"
-	time go test ./shardkv -race -timeout 20m -v
+	time go test ./shardkv -race -timeout 20m -v -count=1
 done 2>&1 | tee test.log
